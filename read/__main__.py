d313711f895d43d38c8c8c29c27a386444213e4a
zgyorffy <zsolt.gyorffy@fortech.ro>
2018-06-08 10:20:28 +0300

    pluralsight tutorial

---
+++ read/__main__.py
@@ -0,0 +1,10 @@
+import sys
+
+import reader
+
+r = reader.Reader(sys.argv[1])
+
+try:
+    print(r.read())
+finally:
+    r.close()
---
+++ read/reader/__init__.py
@@ -0,0 +1,3 @@
+# File: __init__.py
+from reader.reader import Reader
+#print('reader imported')
